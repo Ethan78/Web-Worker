2d7e6b04c0c9de7cd9938f1414d80968ab734972
イーサン <ethan@Eth>
2016-01-05 22:09:40 +0900

    WebWorkerJSONテスト

---
+++ routing.js
@@ -0,0 +1,10 @@
+﻿var webworker = angular.module('webworker', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/index.html");
+    $stateProvider
+.state('testworker', {
+    url: "/testworker",
+    templateUrl: "webworker.html",
+    controller: "webWorkerCtrl"
+});
+});
